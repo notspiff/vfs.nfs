711b09d9b7e9e9f631105bb864e4b49d6538f1ea
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-01-07 15:50:29 +0100

    s

--- src/NFSFile.cpp
+++ src/NFSFile.cpp
@@ -88,7 +88,6 @@ static bool GetServerList(std::vector<VFSDirEntry>& items)
     pItem.path = strdup(path.c_str());
     pItem.label = strdup(currentExport.c_str());
     pItem.title = NULL;
-    pItem.mtime=0;
 
     pItem.folder = true;
     pItem.properties = NULL;
@@ -607,10 +606,10 @@ void* GetDirectory(VFSURL* url,VFSDirEntry** items, int* num_items)
         lTimeDate = nfsdirent->ctime.tv_sec;
       }
 
-      LONGLONG ll = Int32x32To64(lTimeDate & 0xffffffff, 10000000) + 116444736000000000ll;
+      LONGLONG ll = PLATFORM::Int32x32To64(lTimeDate & 0xffffffff, 10000000) + 116444736000000000ll;
       fileTime.dwLowDateTime = (DWORD) (ll & 0xffffffff);
       fileTime.dwHighDateTime = (DWORD)(ll >> 32);
-      FileTimeToLocalFileTime(&fileTime, &localTime);
+      PLATFORM::FileTimeToLocalFileTime(&fileTime, &localTime);
 
       VFSDirEntry pItem;
       pItem.label = strdup(nfsdirent->name);
