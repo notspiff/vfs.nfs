825e257ca6ee496bb05bfedc1bffc5469d724be6
Lukas Rusak <lorusak@gmail.com>
2017-09-24 18:22:25 -0700

    Change to new C++ based addon interface style

--- FindNFS.cmake
+++ FindNFS.cmake
@@ -1,22 +1,37 @@
-# - Try to find libnfs
-# Once done this will define
+#.rst:
+# FindNFS
+# -------
+# Finds the libnfs library
+#
+# This will will define the following variables::
 #
 # NFS_FOUND - system has libnfs
 # NFS_INCLUDE_DIRS - the libnfs include directory
-# NFS_LIBRARIES - The libnfs libraries
+# NFS_LIBRARIES - the libnfs libraries
+# NFS_DEFINITIONS - the libnfs compile definitions
 
-find_package(PkgConfig REQUIRED)
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules (NFS libnfs)
-  list(APPEND NFS_INCLUDE_DIRS ${NFS_INCLUDEDIR})
-else()
-  find_path(NFS_INCLUDE_DIRS nfsc/libnfs.h)
-  find_library(NFS_LIBRARIES nfs)
+  pkg_check_modules(PC_NFS libnfs QUIET)
 endif()
 
+find_path(NFS_INCLUDE_DIR nfsc/libnfs.h
+                          PATHS ${PC_NFS_INCLUDEDIR})
+
+set(NFS_VERSION ${PC_NFS_VERSION})
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NFS DEFAULT_MSG NFS_INCLUDE_DIRS NFS_LIBRARIES)
 
-list(APPEND NFS_DEFINITIONS -DHAVE_LIBNFS=1)
+find_library(NFS_LIBRARY NAMES nfs
+                         PATHS ${PC_NFS_LIBDIR})
 
-mark_as_advanced(NFS_INCLUDE_DIRS NFS_LIBRARIES NFS_DEFINITIONS)
+find_package_handle_standard_args(NFS
+                                  REQUIRED_VARS NFS_LIBRARY NFS_INCLUDE_DIR
+                                  VERSION_VAR NFS_VERSION)
+
+if(NFS_FOUND)
+  set(NFS_LIBRARIES ${NFS_LIBRARY})
+  set(NFS_INCLUDE_DIRS ${NFS_INCLUDE_DIR})
+  set(NFS_DEFINITIONS -DHAVE_LIBNFS=1)
+endif()
+
+mark_as_advanced(NFS_INCLUDE_DIR NFS_LIBRARY)
--- src/NFSConnection.cpp
+++ src/NFSConnection.cpp
@@ -24,7 +24,12 @@ extern "C"
 #include <nfsc/libnfs.h>
 #include <nfsc/libnfs-raw-mount.h>
 }
-#include <kodi/libXBMC_addon.h>
+
+#include <p8-platform/util/timeutils.h>
+
+#include <kodi/Filesystem.h>
+#include <kodi/General.h>
+#include <kodi/Network.h>
 
 //KEEP_ALIVE_TIMEOUT is decremented every half a second
 //360 * 0.5s == 180s == 3mins
@@ -40,8 +45,6 @@ extern "C"
 #define CONTEXT_NEW      1    //new context created
 #define CONTEXT_CACHED   2    //context cached and therefore already mounted (no new mount needed)
 
-extern ADDON::CHelper_libXBMC_addon* XBMC;
-
 CNFSConnection& CNFSConnection::Get()
 {
   static CNFSConnection instance;
@@ -50,7 +53,7 @@ CNFSConnection& CNFSConnection::Get()
 }
 
 CNFSConnection::CNFSConnection()
-: m_pNfsContext(NULL)
+: m_pNfsContext(nullptr)
 , m_exportPath("")
 , m_hostName("")
 , m_resolvedHostName("")
@@ -68,12 +71,9 @@ CNFSConnection::~CNFSConnection()
 }
 
 void CNFSConnection::resolveHost(const std::string& hostname)
-{ 
+{
   //resolve if hostname has changed
-  char* resolved = XBMC->DNSLookup(hostname.c_str());
-  if (resolved)
-    m_resolvedHostName = resolved;
-  free(resolved);
+  kodi::network::DNSLookup(hostname, m_resolvedHostName);
 }
 
 std::list<std::string> CNFSConnection::GetExportList()
@@ -84,10 +84,10 @@ std::list<std::string> CNFSConnection::GetExportList()
   exportlist = mount_getexports(m_resolvedHostName.c_str());
   tmp = exportlist;
 
-  for(tmp = exportlist; tmp!=NULL; tmp=tmp->ex_next)
+  for(tmp = exportlist; tmp!=nullptr; tmp=tmp->ex_next)
   {
     retList.push_back(std::string(tmp->ex_dir));
-  }      
+  }
 
   mount_free_export_list(exportlist);
   retList.sort();
@@ -98,21 +98,25 @@ std::list<std::string> CNFSConnection::GetExportList()
 
 void CNFSConnection::clearMembers()
 {
+  // NOTE - DON'T CLEAR m_exportList HERE!
+  // splitUrlIntoExportAndPath checks for m_exportList.empty()
+  // and would query the server in an excessive unwanted fashion
+  // also don't clear m_KeepAliveTimeouts here because we
+  // would loose any "paused" file handles during export change
   m_exportPath.clear();
   m_hostName.clear();
   m_exportList.clear();
   m_writeChunkSize = 0;
-  m_readChunkSize = 0;  
-  m_pNfsContext = NULL;
-  m_KeepAliveTimeouts.clear();
+  m_readChunkSize = 0;
+  m_pNfsContext = nullptr;
 }
 
 void CNFSConnection::destroyOpenContexts()
 {
   m_openContextLock.Lock();
-  for(tOpenContextMap::iterator it = m_openContextMap.begin();it!=m_openContextMap.end();it++)
+  for (const auto& entry : m_openContextMap)
   {
-    nfs_destroy_context(it->second.pContext);
+    nfs_destroy_context(entry.second.pContext);
   }
   m_openContextMap.clear();
   m_openContextLock.Unlock();
@@ -121,8 +125,8 @@ void CNFSConnection::destroyOpenContexts()
 void CNFSConnection::destroyContext(const std::string& exportName)
 {
   m_openContextLock.Lock();
-  tOpenContextMap::iterator it = m_openContextMap.find(exportName.c_str());
-  if (it != m_openContextMap.end()) 
+  auto it = m_openContextMap.find(exportName);
+  if (it != m_openContextMap.end())
   {
     nfs_destroy_context(it->second.pContext);
     m_openContextMap.erase(it);
@@ -132,29 +136,29 @@ void CNFSConnection::destroyContext(const std::string& exportName)
 
 struct nfs_context *CNFSConnection::getContextFromMap(const std::string& exportname, bool forceCacheHit/* = false*/)
 {
-  struct nfs_context *pRet = NULL;
+  struct nfs_context *pRet = nullptr;
   m_openContextLock.Lock();
 
-  tOpenContextMap::iterator it = m_openContextMap.find(exportname.c_str());
+  auto it = m_openContextMap.find(exportname);
   if(it != m_openContextMap.end())
   {
     //check if context has timed out already
-    uint64_t now = PLATFORM::GetTimeMs();
+    uint64_t now = P8PLATFORM::GetTimeMs();
     if((now - it->second.lastAccessedTime) < CONTEXT_TIMEOUT || forceCacheHit)
     {
       //its not timedout yet or caller wants the cached entry regardless of timeout
       //refresh access time of that
       //context and return it
       if (!forceCacheHit) // only log it if this isn't the resetkeepalive on each read ;)
-        XBMC->Log(ADDON::LOG_DEBUG, "NFS: Refreshing context for %s, old: %" PRId64 ", new: %" PRId64, exportname.c_str(), it->second.lastAccessedTime, now);
+        kodi::Log(ADDON_LOG_DEBUG, "NFS: Refreshing context for %s, old: %" PRId64 ", new: %" PRId64, exportname.c_str(), it->second.lastAccessedTime, now);
       it->second.lastAccessedTime = now;
       pRet = it->second.pContext;
     }
-    else 
+    else
     {
       //context is timed out
-      //destroy it and return NULL
-      XBMC->Log(ADDON::LOG_DEBUG, "NFS: Old context timed out - destroying it");
+      //destroy it and return nullptr
+      kodi::Log(ADDON_LOG_DEBUG, "NFS: Old context timed out - destroying it");
       nfs_destroy_context(it->second.pContext);
       m_openContextMap.erase(it);
     }
@@ -165,28 +169,28 @@ struct nfs_context *CNFSConnection::getContextFromMap(const std::string& exportn
 
 int CNFSConnection::getContextForExport(const std::string& exportname)
 {
-  int ret = CONTEXT_INVALID; 
-    
-  clearMembers();  
-    
+  int ret = CONTEXT_INVALID;
+
+  clearMembers();
+
   m_pNfsContext = getContextFromMap(exportname);
 
   if(!m_pNfsContext)
   {
-    XBMC->Log(ADDON::LOG_DEBUG,"NFS: Context for %s not open - get a new context.", exportname.c_str());
+    kodi::Log(ADDON_LOG_DEBUG,"NFS: Context for %s not open - get a new context.", exportname.c_str());
     m_pNfsContext = nfs_init_context();
-    
-    if(!m_pNfsContext) 
+
+    if(!m_pNfsContext)
     {
-      XBMC->Log(ADDON::LOG_ERROR,"NFS: Error initcontext in getContextForExport.");
+      kodi::Log(ADDON_LOG_ERROR,"NFS: Error initcontext in getContextForExport.");
     }
-    else 
+    else
     {
       struct contextTimeout tmp;
       m_openContextLock.Lock();
       tmp.pContext = m_pNfsContext;
-      tmp.lastAccessedTime = PLATFORM::GetTimeMs();
-      m_openContextMap[exportname] = tmp; //add context to list of all contexts      
+      tmp.lastAccessedTime = P8PLATFORM::GetTimeMs();
+      m_openContextMap[exportname] = tmp; //add context to list of all contexts
       ret = CONTEXT_NEW;
       m_openContextLock.Unlock();
     }
@@ -194,9 +198,9 @@ int CNFSConnection::getContextForExport(const std::string& exportname)
   else
   {
     ret = CONTEXT_CACHED;
-    XBMC->Log(ADDON::LOG_DEBUG,"NFS: Using cached context.");
+    kodi::Log(ADDON_LOG_DEBUG,"NFS: Using cached context.");
   }
-  m_lastAccessedTime = PLATFORM::GetTimeMs(); //refresh last access time of m_pNfsContext
+  m_lastAccessedTime = P8PLATFORM::GetTimeMs(); //refresh last access time of m_pNfsContext
 
   return ret;
 }
@@ -207,7 +211,7 @@ bool CNFSConnection::splitUrlIntoExportAndPath(const std::string& hostname,
                                                std::string& relativePath)
 {
   bool ret = false;
-    
+
   //refresh exportlist if empty or hostname change
   if(m_exportList.empty() || m_hostName != hostname)
   {
@@ -218,9 +222,9 @@ bool CNFSConnection::splitUrlIntoExportAndPath(const std::string& hostname,
   {
     relativePath = "";
     exportPath = "";
-      
+
     std::string path = filename;
-      
+
     //GetFileName returns path without leading "/"
     //but we need it because the export paths start with "/"
     //and path.Find(*it) wouldn't work else
@@ -228,77 +232,76 @@ bool CNFSConnection::splitUrlIntoExportAndPath(const std::string& hostname,
     {
       path = "/" + path;
     }
-      
+
     std::list<std::string>::iterator it;
-      
-    for(it=m_exportList.begin();it!=m_exportList.end();it++)
+
+    for (const auto& entry : m_exportList)
     {
       //if path starts with the current export path
-      if(path.compare(0, it->size(), *it) == 0)
+      if(path.compare(0, entry.size(), entry) == 0)
       {
-        exportPath = *it;
+        exportPath = entry;
         //handle special case where root is exported
-        //in that case we don't want to stripp off to
+        //in that case we don't want to strip off to
         //much from the path
         if( exportPath == "/" )
           relativePath = "//" + path.substr(exportPath.length()-1);
         else
           relativePath = "//" + path.substr(exportPath.length());
         ret = true;
-        break;          
+        break;
       }
     }
   }
   return ret;
 }
 
-bool CNFSConnection::Connect(VFSURL* url, std::string& relativePath)
+bool CNFSConnection::Connect(const VFSURL& url, std::string& relativePath)
 {
-  PLATFORM::CLockObject lock(*this);
-  bool ret = false;
+  P8PLATFORM::CLockObject lock(*this);
   int nfsRet = 0;
   std::string exportPath;
 
-  resolveHost(url->hostname);
-  ret = splitUrlIntoExportAndPath(url->hostname, url->filename, exportPath, relativePath);
-  
-  if( (ret && (exportPath != m_exportPath  || 
-      m_hostName != url->hostname))    ||
-      (PLATFORM::GetTimeMs() - m_lastAccessedTime) > CONTEXT_TIMEOUT )
+  resolveHost(url.hostname);
+  bool ret = splitUrlIntoExportAndPath(url.hostname, url.filename, exportPath, relativePath);
+
+  if( (ret && (exportPath != m_exportPath  ||
+      m_hostName != url.hostname))    ||
+      (P8PLATFORM::GetTimeMs() - m_lastAccessedTime) > CONTEXT_TIMEOUT )
   {
-    int contextRet = getContextForExport(std::string(url->hostname) + exportPath);
-    
+    int contextRet = getContextForExport(std::string(url.hostname) + exportPath);
+
     if(contextRet == CONTEXT_INVALID)//we need a new context because sharename or hostname has changed
     {
       return false;
     }
-    
+
     if(contextRet == CONTEXT_NEW) //new context was created - we need to mount it
     {
       //we connect to the directory of the path. This will be the "root" path of this connection then.
       //So all fileoperations are relative to this mountpoint...
       nfsRet = nfs_mount(m_pNfsContext, m_resolvedHostName.c_str(), exportPath.c_str());
 
-      if(nfsRet != 0) 
+      if(nfsRet != 0)
       {
-        XBMC->Log(ADDON::LOG_ERROR,"NFS: Failed to mount nfs share: %s %s (%s)\n", m_resolvedHostName.c_str(), exportPath.c_str(), nfs_get_error(m_pNfsContext));
-        destroyContext(std::string(url->hostname) + exportPath);
+        kodi::Log(ADDON_LOG_ERROR,"NFS: Failed to mount nfs share: %s %s (%s)", m_resolvedHostName.c_str(), exportPath.c_str(), nfs_get_error(m_pNfsContext));
+        destroyContext(std::string(url.hostname) + exportPath);
         return false;
       }
-      XBMC->Log(ADDON::LOG_DEBUG,"NFS: Connected to server %s and export %s\n", url->hostname, exportPath.c_str());
+      kodi::Log(ADDON_LOG_DEBUG,"NFS: Connected to server %s and export %s", url.hostname, exportPath.c_str());
     }
     m_exportPath = exportPath;
-    m_hostName = url->hostname;
+    m_hostName = url.hostname;
     //read chunksize only works after mount
     m_readChunkSize = nfs_get_readmax(m_pNfsContext);
     m_writeChunkSize =nfs_get_writemax(m_pNfsContext);
 
     if(contextRet == CONTEXT_NEW)
     {
-      XBMC->Log(ADDON::LOG_DEBUG,"NFS: chunks: r/w %i/%i\n", (int)m_readChunkSize,(int)m_writeChunkSize);
+      kodi::Log(ADDON_LOG_DEBUG,"NFS: chunks: r/w %i/%i\n", (int)m_readChunkSize,(int)m_writeChunkSize);
     }
   }
-  return ret; 
+  return ret;
 }
 
 void CNFSConnection::Deinit()
@@ -306,9 +309,11 @@ void CNFSConnection::Deinit()
   if(m_pNfsContext)
   {
     destroyOpenContexts();
-    m_pNfsContext = NULL;
-  }        
+    m_pNfsContext = nullptr;
+  }
   clearMembers();
+  // clear any keep alive timouts on deinit
+  m_KeepAliveTimeouts.clear();
 }
 
 /* This is called from CApplication::ProcessSlow() and is used to tell if nfs have been idle for too long */
@@ -316,9 +321,9 @@ void CNFSConnection::CheckIfIdle()
 {
   /* We check if there are open connections. This is done without a lock to not halt the mainthread. It should be thread safe as
    worst case scenario is that m_OpenConnections could read 0 and then changed to 1 if this happens it will enter the if wich will lead to another check, wich is locked.  */
-  if (m_OpenConnections == 0 && m_pNfsContext != NULL)
-  { /* I've set the the maxiumum IDLE time to be 1 min and 30 sec. */
-    PLATFORM::CLockObject lock(*this);
+  if (m_OpenConnections == 0 && m_pNfsContext != nullptr)
+  { /* I've set the the maximum IDLE time to be 1 min and 30 sec. */
+    P8PLATFORM::CLockObject lock(*this);
     if (m_OpenConnections == 0 /* check again - when locked */)
     {
       if (m_IdleTimeout > 0)
@@ -327,27 +332,27 @@ void CNFSConnection::CheckIfIdle()
       }
       else
       {
-        XBMC->Log(ADDON::LOG_NOTICE, "NFS is idle. Closing the remaining connections.");
+        kodi::Log(ADDON_LOG_NOTICE, "NFS is idle. Closing the remaining connections.");
         Deinit();
       }
     }
   }
-  
-  if( m_pNfsContext != NULL )
+
+  if( m_pNfsContext != nullptr )
   {
-    PLATFORM::CLockObject lock(m_keepAliveLock);
+    P8PLATFORM::CLockObject lock(m_keepAliveLock);
     //handle keep alive on opened files
-    for( tFileKeepAliveMap::iterator it = m_KeepAliveTimeouts.begin();it!=m_KeepAliveTimeouts.end();it++)
+    for (auto& entry : m_KeepAliveTimeouts)
     {
-      if(it->second.refreshCounter > 0)
+      if(entry.second.refreshCounter > 0)
       {
-        it->second.refreshCounter--;
+        entry.second.refreshCounter--;
       }
       else
       {
-        keepAlive(it->second.exportPath, it->first);
+        keepAlive(entry.second.exportPath, entry.first);
         //reset timeout
-        resetKeepAlive(it->second.exportPath, it->first);
+        resetKeepAlive(entry.second.exportPath, entry.first);
       }
     }
   }
@@ -356,16 +361,24 @@ void CNFSConnection::CheckIfIdle()
 //remove file handle from keep alive list on file close
 void CNFSConnection::removeFromKeepAliveList(struct nfsfh  *_pFileHandle)
 {
-  PLATFORM::CLockObject lock(m_keepAliveLock);
+  P8PLATFORM::CLockObject lock(m_keepAliveLock);
   m_KeepAliveTimeouts.erase(_pFileHandle);
 }
 
 //reset timeouts on read
 void CNFSConnection::resetKeepAlive(std::string _exportPath, struct nfsfh  *_pFileHandle)
 {
-  PLATFORM::CLockObject lock(m_keepAliveLock);
+  P8PLATFORM::CLockObject lock(m_keepAliveLock);
   //refresh last access time of the context aswell
-  getContextFromMap(_exportPath, true);
+  struct nfs_context *pContext = getContextFromMap(_exportPath, true);
+
+  // if we keep alive using m_pNfsContext we need to mark
+  // its last access time too here
+  if (m_pNfsContext == pContext)
+  {
+    m_lastAccessedTime = P8PLATFORM::GetTimeMs();
+  }
+
   //adds new keys - refreshs existing ones
   m_KeepAliveTimeouts[_pFileHandle].exportPath = _exportPath;
   m_KeepAliveTimeouts[_pFileHandle].refreshCounter = KEEP_ALIVE_TIMEOUT;
@@ -383,48 +396,48 @@ void CNFSConnection::keepAlive(std::string _exportPath, struct nfsfh  *_pFileHan
   // on this call we are sure its not timedout even if the last accessed
   // time suggests it.
   struct nfs_context *pContext = getContextFromMap(_exportPath, true);
-  
+
   if (!pContext)// this should normally never happen - paranoia
     pContext = m_pNfsContext;
-  
-  XBMC->Log(ADDON::LOG_NOTICE, "NFS: sending keep alive after %i s.",KEEP_ALIVE_TIMEOUT/2);
-  PLATFORM::CLockObject lock(*this);
+
+  kodi::Log(ADDON_LOG_NOTICE, "NFS: sending keep alive after %i s.", KEEP_ALIVE_TIMEOUT/2);
+  P8PLATFORM::CLockObject lock(*this);
   nfs_lseek(pContext, _pFileHandle, 0, SEEK_CUR, &offset);
   nfs_read(pContext, _pFileHandle, 32, buffer);
   nfs_lseek(pContext, _pFileHandle, offset, SEEK_SET, &offset);
 }
 
-int CNFSConnection::stat(VFSURL* url, struct stat *statbuff)
+int CNFSConnection::stat(const VFSURL& url, struct stat *statbuff)
 {
-  PLATFORM::CLockObject lock(*this);
+  P8PLATFORM::CLockObject lock(*this);
   int nfsRet = 0;
   std::string exportPath;
   std::string relativePath;
-  struct nfs_context *pTmpContext = NULL;
-  
-  resolveHost(url->hostname);
-  
-  if(splitUrlIntoExportAndPath(url->hostname, url->filename, exportPath, relativePath))
-  {    
+  struct nfs_context *pTmpContext = nullptr;
+
+  resolveHost(url.hostname);
+
+  if(splitUrlIntoExportAndPath(url.hostname, url.filename, exportPath, relativePath))
+  {
     pTmpContext = nfs_init_context();
-    
+
     if(pTmpContext)
-    {  
+    {
       //we connect to the directory of the path. This will be the "root" path of this connection then.
       //So all fileoperations are relative to this mountpoint...
       nfsRet = nfs_mount(pTmpContext, m_resolvedHostName.c_str(), exportPath.c_str());
-      
-      if(nfsRet == 0) 
+
+      if(nfsRet == 0)
       {
-        nfsRet = nfs_stat(pTmpContext, relativePath.c_str(), statbuff);      
+        nfsRet = nfs_stat(pTmpContext, relativePath.c_str(), statbuff);
       }
       else
       {
-        XBMC->Log(ADDON::LOG_ERROR,"NFS: Failed to mount nfs share: %s (%s)\n", exportPath.c_str(), nfs_get_error(m_pNfsContext));
+        kodi::Log(ADDON_LOG_ERROR,"NFS: Failed to mount nfs share: %s (%s)", exportPath.c_str(), nfs_get_error(m_pNfsContext));
       }
-      
+
       nfs_destroy_context(pTmpContext);
-      XBMC->Log(ADDON::LOG_DEBUG,"NFS: Connected to server %s and export %s in tmpContext\n", url->hostname, exportPath.c_str());
+      kodi::Log(ADDON_LOG_DEBUG,"NFS: Connected to server %s and export %s in tmpContext", url.hostname, exportPath.c_str());
     }
   }
 
@@ -435,13 +448,13 @@ int CNFSConnection::stat(VFSURL* url, struct stat *statbuff)
 needed for unloading the dylib*/
 void CNFSConnection::AddActiveConnection()
 {
-  PLATFORM::CLockObject lock(*this);
+  P8PLATFORM::CLockObject lock(*this);
   m_OpenConnections++;
 }
 
 void CNFSConnection::AddIdleConnection()
 {
-  PLATFORM::CLockObject lock(*this);
+  P8PLATFORM::CLockObject lock(*this);
   m_OpenConnections--;
   /* If we close a file we reset the idle timer so that we don't have any wierd behaviours if a user
    leaves the movie paused for a long while and then press stop */
